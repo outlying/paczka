660b940a22b6c92d90df3361a19114d42f505389
outlying <outlying@gmail.com>
2020-04-24 10:09:36 +0200

    Comments, automerge remove label

--- core/src/main/java/com/antyzero/paczka/core/tracker/HistoryProvider.kt
+++ core/src/main/java/com/antyzero/paczka/core/tracker/HistoryProvider.kt
@@ -1,6 +1,14 @@
 package com.antyzero.paczka.core.tracker
 
+/**
+ * Gathers information about given parcel ID
+ */
 interface HistoryProvider {
 
+    /**
+     * Gathers parcel information, usually from external system
+     *
+     * @param [id], anything provided will be converted to [String] by it's [toString] function
+     */
     suspend fun get(id: Any): String
 }
