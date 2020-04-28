318560b79bfb86e72f21c1315284bf56158f4d80
outlying <outlying@gmail.com>
2020-04-28 19:35:29 +0200

    Things

--- app/src/main/java/com/antyzero/paczka/app/ui/screen/parcels/ParcelsFragment.kt
+++ app/src/main/java/com/antyzero/paczka/app/ui/screen/parcels/ParcelsFragment.kt
@@ -1,31 +1,29 @@
 package com.antyzero.paczka.app.ui.screen.parcels
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.antyzero.paczka.app.R
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class ParcelsFragment : Fragment() {
+class ParcelsFragment : Fragment(R.layout.fragment_parcels) {
 
     private lateinit var parcelsViewModel: ParcelsViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         parcelsViewModel =
             ViewModelProvider(this).get(ParcelsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_parcels, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        parcelsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+
+        lifecycleScope.launch {
+            parcelsViewModel.quote.collect {
+                Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/antyzero/paczka/app/ui/screen/parcels/ParcelsViewModel.kt
+++ app/src/main/java/com/antyzero/paczka/app/ui/screen/parcels/ParcelsViewModel.kt
@@ -1,13 +1,17 @@
 package com.antyzero.paczka.app.ui.screen.parcels
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.launch
 
 class ParcelsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is parcels Fragment"
-    }
-    val text: LiveData<String> = _text
+    private val _quote = ConflatedBroadcastChannel<String>()
+
+    val quote: Flow<String>
+        get() = _quote.asFlow()
 }
