3cf912c74948e86fe4629b61e4c772f0eb60c65c
Iwo Polański <outlying@gmail.com>
2020-04-29 20:02:32 +0200

    Working items

--- app/src/main/java/com/antyzero/paczka/app/ui/adapter/ParcelAdapter.kt
+++ app/src/main/java/com/antyzero/paczka/app/ui/adapter/ParcelAdapter.kt
@@ -1,31 +1,22 @@
 package com.antyzero.paczka.app.ui.adapter
 
-import android.graphics.Color
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import com.antyzero.paczka.app.R
 import com.antyzero.paczka.app.ui.model.ParcelUi
-import kotlin.random.Random
 
 object ParcelAdapter :
     ListAdapter<ParcelUi.Simple, SimpleParcelViewHolder>(SimpleParcelItemCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleParcelViewHolder {
         return SimpleParcelViewHolder(
-            View.inflate(
-                parent.context,
-                R.layout.item_parcel_simple,
-                null
-            ).apply {
-                setBackgroundColor(
-                    Color.rgb(
-                        Random.nextInt(255),
-                        Random.nextInt(255),
-                        Random.nextInt(255)
-                    )
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.item_parcel_simple,
+                    parent,
+                    false
                 )
-            }
         )
     }
 
--- app/src/main/java/com/antyzero/paczka/app/ui/adapter/SimpleParcelViewHolder.kt
+++ app/src/main/java/com/antyzero/paczka/app/ui/adapter/SimpleParcelViewHolder.kt
@@ -1,12 +1,31 @@
 package com.antyzero.paczka.app.ui.adapter
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.antyzero.paczka.app.R
 import com.antyzero.paczka.app.ui.model.ParcelUi
+import kotlin.random.Random
 
 class SimpleParcelViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
+    private val textView = view.findViewById<TextView>(R.id.textView)
+
+    init {
+
+        view.apply {
+            setBackgroundColor(
+                Color.rgb(
+                    Random.nextInt(255),
+                    Random.nextInt(255),
+                    Random.nextInt(255)
+                )
+            )
+        }
+    }
+
     fun bind(parcelUi: ParcelUi.Simple) {
-        
+        textView.text = parcelUi.id.toString()
     }
 }
